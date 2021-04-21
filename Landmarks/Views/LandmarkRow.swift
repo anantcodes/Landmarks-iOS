fa150b6ac556ecbd59d40bcd9b644bd801ee99c6
Anant Kanchan <anant4265@gmail.com>
2021-04-21 16:40:39 +0530

    LandmarkRow initial file added

---
+++ Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Anant Kanchan on 21/04/21.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow()
+    }
+}
